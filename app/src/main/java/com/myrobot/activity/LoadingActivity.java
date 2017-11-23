47241454f3b85709a778cdb7b32625806317bd8e
yangsong <yang3384046@126.com>
2017-11-23 18:00:26 +0800

    11.23

--- app/src/main/java/com/myrobot/activity/LoadingActivity.java
+++ app/src/main/java/com/myrobot/activity/LoadingActivity.java
@@ -1,11 +1,15 @@
 package com.myrobot.activity;
 
+import android.content.Intent;
+import android.os.Handler;
+
 import com.myrobot.R;
 import com.myrobot.base.BaseActivity;
 
 public class LoadingActivity extends BaseActivity {
 
 
+    Handler handler = new Handler();
 
     @Override
     protected int getContentView() {
@@ -14,6 +18,11 @@ public class LoadingActivity extends BaseActivity {
 
     @Override
     protected void init() {
-
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingActivity.this, MainActivity.class));
+            }
+        }, 20000);
     }
 }
