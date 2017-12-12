e907420ca4860291a433e4f0b1c7ae18bbd5992d
yangsong <yang3384046@126.com>
2017-12-12 17:00:40 +0800

    12.12

--- app/src/main/java/com/myrobot/activity/HuiYiActivity.java
+++ app/src/main/java/com/myrobot/activity/HuiYiActivity.java
@@ -1,23 +1,14 @@
 package com.myrobot.activity;
 
+import android.content.ComponentName;
 import android.content.Intent;
-import android.os.Bundle;
 
 import com.inpor.fastmeetingcloud.ui.StartTheMiddleTierActivity;
+import com.inpor.fastmeetingcloud.util.Constant;
 import com.myrobot.R;
 import com.myrobot.base.BaseActivity;
 
 public class HuiYiActivity extends BaseActivity {
-
-
-    String BUNDLE_USERNAME = "userName";
-    String BUNDLE_PASSWORD = "userPwd";
-    String BUNDLE_SERVER_ADDRESS = "svrAddress";
-    String BUNDLE_SERVER_PORT = "svrPort";
-    String BUNDLE_SERVER_ROOMID = "roomId";
-    String BUNDLE_USERID = "userId";
-    String BUNDLE_NICKNAME = "nickname";
-
     @Override
     protected int getContentView() {
         return R.layout.activity_hui_yi;
@@ -29,18 +20,17 @@ public class HuiYiActivity extends BaseActivity {
         String password = "123456";
         String port = "1089";
         String serverAddress = "a.fsmeeting.com";
-        String userId = "10005";
-        Intent intent = new Intent(HuiYiActivity.this, StartTheMiddleTierActivity.class);
-
-        intent.setAction("intent_app_action");
-        Bundle bundle = new Bundle();
-        bundle.putString(BUNDLE_USERNAME, userName);
-        bundle.putString(BUNDLE_PASSWORD, password);
-        bundle.putString(BUNDLE_SERVER_PORT, port);
-        bundle.putString(BUNDLE_SERVER_ADDRESS, serverAddress);
-        bundle.putString(BUNDLE_USERID, userId);
-        intent.putExtras(bundle);
-        startActivity(intent);
+        ComponentName apk2Component1 = new ComponentName("com.myrobot", "com.inpor.fastmeetingcloud.ui.StartTheMiddleTierActivity");
+        Intent mIntent = new Intent();
+        mIntent.setAction(Constant.INTENT_APP_ACTION);
+        mIntent.putExtra(StartTheMiddleTierActivity.ThirdLoginConstant.BUNDLE_USERNAME, userName);  //必传
+        mIntent.putExtra(StartTheMiddleTierActivity.ThirdLoginConstant.BUNDLE_PASSWORD, password);//必传
+       // mIntent.putExtra(StartTheMiddleTierActivity.ThirdLoginConstant.BUNDLE_ROOMID, et_roomid.getText().toString().trim());//选传
+       // mIntent.putExtra(StartTheMiddleTierActivity.ThirdLoginConstant.BUNDLE_ROOMPASSWD, "f1234567890");//选传
+        mIntent.putExtra(StartTheMiddleTierActivity.ThirdLoginConstant.BUNDLE_SERVER_ADDRESS, serverAddress);//必传
+        mIntent.putExtra(StartTheMiddleTierActivity.ThirdLoginConstant.BUNDLE_SERVER_PORT, port);//必传
+        mIntent.setComponent(apk2Component1);
+        startActivity(mIntent);
         finish();
     }
 }
