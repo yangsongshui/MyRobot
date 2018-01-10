2ccc8a4eae195e23f480d29b61e821356227eaeb
yangsongshui <yang3384046@126.com>
2018-01-11 00:30:46 +0800

    1.10

--- app/src/main/java/com/myrobot/activity/MainActivity.java
+++ app/src/main/java/com/myrobot/activity/MainActivity.java
@@ -1,20 +1,35 @@
 package com.myrobot.activity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.myrobot.R;
 import com.myrobot.api.User;
 import com.myrobot.base.BaseActivity;
+import com.myrobot.bean.AssistBean;
+import com.myrobot.bean.ComBean;
+import com.myrobot.utils.MD5;
+import com.myrobot.utils.SerialHelper;
+import com.myrobot.utils.SpUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.security.InvalidParameterException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -40,6 +55,10 @@ public class MainActivity extends BaseActivity {
     Gson gs;
     private ProgressDialog progressDialog;
     Handler handler;
+    SerialControl ComA;
+    DispQueueThread DispQueue;//刷新显示线程
+    String userid = "";
+    int id = 0;
 
     @Override
     protected int getContentView() {
@@ -52,6 +71,12 @@ public class MainActivity extends BaseActivity {
         progressDialog.setMessage("加载中...");
         client = new OkHttpClient();
         gs = new Gson();
+        ComA = new SerialControl();
+        DispQueue = new DispQueueThread();
+        DispQueue.start();
+        ComA.setPort("/dev/ttyS4");
+        ComA.setBaudRate("9600");
+        OpenComPort(ComA);
         handler = new Handler(new Handler.Callback() {
             @Override
             public boolean handleMessage(Message msg) {
@@ -65,13 +90,13 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-    @OnClick({R.id.login_bt, R.id.zhuche_bt, R.id.main_back,R.id.main_rl})
+    @OnClick({R.id.login_bt, R.id.zhuche_bt, R.id.main_back, R.id.main_rl, R.id.zhiwenbt})
     public void onViewClicked(View view) {
         play();
         switch (view.getId()) {
             case R.id.login_bt:
-                startActivity(new Intent(MainActivity.this, HomeActivity.class));
-             /*   if (!progressDialog.isShowing()) {
+                // startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                if (!progressDialog.isShowing()) {
                     progressDialog.show();
                 }
                 String phone = phoneEt.getText().toString();
@@ -80,7 +105,7 @@ public class MainActivity extends BaseActivity {
                 jsonObject.addProperty("phone", phone);
                 jsonObject.addProperty("password", MD5.getMD5(psw));
                 post("http://112.74.196.237:81/robot_api/public/index.php/users/login?", jsonObject.toString());
-*/
+
                 break;
             case R.id.zhuche_bt:
 
@@ -92,6 +117,9 @@ public class MainActivity extends BaseActivity {
                 break;
             case R.id.main_rl:
                 rl.setVisibility(View.VISIBLE);
+                break;
+            case R.id.zhiwenbt:
+                progressDialog.show();
                 break;
             default:
                 break;
@@ -135,5 +163,145 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+    }
+
+    //----------------------------------------------------串口控制类
+    private class SerialControl extends SerialHelper {
+
+        //		public SerialControl(String sPort, String sBaudRate){
+//			super(sPort, sBaudRate);
+//		}
+        public SerialControl() {
+        }
+
+        @Override
+        protected void onDataReceived(final ComBean ComRecData) {
+            //数据接收量大或接收时弹出软键盘，界面会卡顿,可能和6410的显示性能有关
+            //直接刷新显示，接收数据量大时，卡顿明显，但接收与显示同步。
+            //用线程定时刷新显示可以获得较流畅的显示效果，但是接收数据速度快于显示速度时，显示会滞后。
+            //最终效果差不多-_-，线程定时刷新稍好一些。
+            //线程定时刷新显示(推荐)
+            DispQueue.AddQueue(ComRecData);
+            /*
+            runOnUiThread(new Runnable()//直接刷新显示
+			{
+				public void run()
+				{
+					DispRecData(ComRecData);
+				}
+			});*/
+        }
+    }
+
+    //----------------------------------------------------刷新显示线程
+    private class DispQueueThread extends Thread {
+        private Queue<ComBean> QueueList = new LinkedList<ComBean>();
+
+        @Override
+        public void run() {
+            super.run();
+            while (!isInterrupted()) {
+                final ComBean ComData;
+                while ((ComData = QueueList.poll()) != null) {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            DispRecData(ComData);
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);//显示性能高的话，可以把此数值调小。
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        public synchronized void AddQueue(ComBean ComData) {
+            QueueList.add(ComData);
+        }
+    }
+
+    //----------------------------------------------------显示接收数据
+    private void DispRecData(ComBean ComRecData) {
+        StringBuilder sMsg = new StringBuilder();
+        sMsg.append("[");
+        sMsg.append(ComRecData.sComPort);
+        sMsg.append("]");
+        sMsg.append(new String(ComRecData.bRec));
+        Log.e("收到数据", sMsg.toString());
+        if (sMsg.indexOf("@") != -1) {
+            String msg = sMsg.substring(sMsg.indexOf("@") + 1, sMsg.length());
+            String userid = msg.substring(1, 3);
+            if (msg.substring(9, 10).equals("3")) {
+                String phone = SpUtils.getString(userid + "phone", "");
+                String psw = SpUtils.getString(userid + "psw", "");
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("phone", phone);
+                jsonObject.addProperty("password", MD5.getMD5(psw));
+                post("http://112.74.196.237:81/robot_api/public/index.php/users/login?", jsonObject.toString());
+            } else {
+                showToastor("指纹识别失败");
+                if (progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+
+            }
+        }
+
+
+    }
+
+    private AssistBean getAssistData() {
+        SharedPreferences msharedPreferences = getSharedPreferences("ComAssistant", Context.MODE_PRIVATE);
+        AssistBean AssistData = new AssistBean();
+        try {
+            String personBase64 = msharedPreferences.getString("AssistData", "");
+            byte[] base64Bytes = Base64.decode(personBase64.getBytes(), 0);
+            ByteArrayInputStream bais = new ByteArrayInputStream(base64Bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            AssistData = (AssistBean) ois.readObject();
+            return AssistData;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return AssistData;
+    }
+
+    //----------------------------------------------------关闭串口
+    private void CloseComPort(SerialHelper ComPort) {
+        if (ComPort != null) {
+            ComPort.stopSend();
+            ComPort.close();
+        }
+    }
+
+    //----------------------------------------------------打开串口
+    private void OpenComPort(SerialHelper ComPort) {
+        try {
+            ComPort.open();
+
+        } catch (SecurityException e) {
+            showToastor("打开串口失败:没有串口读/写权限!");
+        } catch (IOException e) {
+            showToastor("打开串口失败:未知错误!");
+        } catch (InvalidParameterException e) {
+            showToastor("打开串口失败:参数错误!");
+        }
+    }
+
+    //----------------------------------------------------串口发送
+    private void sendPortData(SerialHelper ComPort, String sOut) {
+        if (ComPort != null && ComPort.isOpen()) {
+            ComPort.sendTxt(sOut);
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CloseComPort(ComA);
     }
 }
--- app/src/main/java/com/myrobot/activity/ZhuCeActivity.java
+++ app/src/main/java/com/myrobot/activity/ZhuCeActivity.java
@@ -1,8 +1,12 @@
 package com.myrobot.activity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -16,9 +20,18 @@ import com.myrobot.R;
 import com.myrobot.api.Code;
 import com.myrobot.api.User;
 import com.myrobot.base.BaseActivity;
+import com.myrobot.bean.AssistBean;
+import com.myrobot.bean.ComBean;
 import com.myrobot.utils.MD5;
+import com.myrobot.utils.SerialHelper;
+import com.myrobot.utils.SpUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.security.InvalidParameterException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -47,6 +60,10 @@ public class ZhuCeActivity extends BaseActivity {
     OkHttpClient client;
     Gson gs;
     Handler handler;
+    SerialControl ComA;
+    DispQueueThread DispQueue;//刷新显示线程
+    String userid = "";
+    int id = 0;
 
     @Override
     protected int getContentView() {
@@ -57,6 +74,19 @@ public class ZhuCeActivity extends BaseActivity {
     protected void init() {
         gs = new Gson();
         client = new OkHttpClient();
+        ComA = new SerialControl();
+        DispQueue = new DispQueueThread();
+        DispQueue.start();
+        ComA.setPort("/dev/ttyS4");
+        ComA.setBaudRate("9600");
+        OpenComPort(ComA);
+        id = SpUtils.getInt("id", 0);
+
+        if (id < 10) {
+            userid = "0" + 1;
+        } else {
+            userid = String.valueOf(id);
+        }
         handler = new Handler(new Handler.Callback() {
             @Override
             public boolean handleMessage(Message msg) {
@@ -64,11 +94,28 @@ public class ZhuCeActivity extends BaseActivity {
                     String js = msg.getData().getString("user");
                     User user = gs.fromJson(js, User.class);//把JSON字符串转为对象
                     showToastor(user.getMsg());
-                    if (user.getCode() == 1)
-                        finish();
+                    if (user.getCode() == 1) {
+                        sendPortData(ComA, "[51000000000000@10000" + userid + "]");
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                sendPortData(ComA, "[51000000000000@10000" + userid + "]");
+                            }
+                        }, 200);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                sendPortData(ComA, "[51000000000000@10000" + userid + "]");
+                            }
+                        }, 400);
+                        SpUtils.putString(userid + "phone", phoneEt.getText().toString().trim());
+                        SpUtils.putString(userid + "psw", pswEt.getText().toString().trim());
+                        id++;
+                        SpUtils.putInt("id", id);
+                    }
                 } else if (msg.arg1 == 2) {
                     showToastor("网络异常");
-                }else if (msg.arg1==3){
+                } else if (msg.arg1 == 3) {
                     showToastor((String) msg.obj);
                 }
                 relativeLayout.setVisibility(View.VISIBLE);
@@ -77,6 +124,7 @@ public class ZhuCeActivity extends BaseActivity {
                 return false;
             }
         });
+
 
     }
 
@@ -137,7 +185,7 @@ public class ZhuCeActivity extends BaseActivity {
                 jsonObject = gs.fromJson(js, Code.class);//把JSON字符串转为对象
                 Message msg = new Message();
                 msg.arg1 = 3;
-                msg.obj=jsonObject.getMsg();
+                msg.obj = jsonObject.getMsg();
                 handler.sendMessage(msg);
             }
         });
@@ -169,4 +217,128 @@ public class ZhuCeActivity extends BaseActivity {
             }
         });
     }
+
+    //----------------------------------------------------串口控制类
+    private class SerialControl extends SerialHelper {
+
+        //		public SerialControl(String sPort, String sBaudRate){
+//			super(sPort, sBaudRate);
+//		}
+        public SerialControl() {
+        }
+
+        @Override
+        protected void onDataReceived(final ComBean ComRecData) {
+            //数据接收量大或接收时弹出软键盘，界面会卡顿,可能和6410的显示性能有关
+            //直接刷新显示，接收数据量大时，卡顿明显，但接收与显示同步。
+            //用线程定时刷新显示可以获得较流畅的显示效果，但是接收数据速度快于显示速度时，显示会滞后。
+            //最终效果差不多-_-，线程定时刷新稍好一些。
+            //线程定时刷新显示(推荐)
+            DispQueue.AddQueue(ComRecData);
+            /*
+            runOnUiThread(new Runnable()//直接刷新显示
+			{
+				public void run()
+				{
+					DispRecData(ComRecData);
+				}
+			});*/
+        }
+    }
+
+    //----------------------------------------------------刷新显示线程
+    private class DispQueueThread extends Thread {
+        private Queue<ComBean> QueueList = new LinkedList<ComBean>();
+
+        @Override
+        public void run() {
+            super.run();
+            while (!isInterrupted()) {
+                final ComBean ComData;
+                while ((ComData = QueueList.poll()) != null) {
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            DispRecData(ComData);
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);//显示性能高的话，可以把此数值调小。
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+        }
+
+        public synchronized void AddQueue(ComBean ComData) {
+            QueueList.add(ComData);
+        }
+    }
+
+    //----------------------------------------------------显示接收数据
+    private void DispRecData(ComBean ComRecData) {
+        StringBuilder sMsg = new StringBuilder();
+        sMsg.append("[");
+        sMsg.append(ComRecData.sComPort);
+        sMsg.append("]");
+        sMsg.append(new String(ComRecData.bRec));
+        Log.e("收到数据", sMsg.toString());
+        if (sMsg.indexOf("@") != -1)
+            finish();
+
+    }
+
+    private AssistBean getAssistData() {
+        SharedPreferences msharedPreferences = getSharedPreferences("ComAssistant", Context.MODE_PRIVATE);
+        AssistBean AssistData = new AssistBean();
+        try {
+            String personBase64 = msharedPreferences.getString("AssistData", "");
+            byte[] base64Bytes = Base64.decode(personBase64.getBytes(), 0);
+            ByteArrayInputStream bais = new ByteArrayInputStream(base64Bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            AssistData = (AssistBean) ois.readObject();
+            return AssistData;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return AssistData;
+    }
+
+    //----------------------------------------------------关闭串口
+    private void CloseComPort(SerialHelper ComPort) {
+        if (ComPort != null) {
+            ComPort.stopSend();
+            ComPort.close();
+        }
+    }
+
+    //----------------------------------------------------打开串口
+    private void OpenComPort(SerialHelper ComPort) {
+        try {
+            ComPort.open();
+
+        } catch (SecurityException e) {
+            showToastor("打开串口失败:没有串口读/写权限!");
+        } catch (IOException e) {
+            showToastor("打开串口失败:未知错误!");
+        } catch (InvalidParameterException e) {
+            showToastor("打开串口失败:参数错误!");
+        }
+    }
+
+    //----------------------------------------------------串口发送
+    private void sendPortData(SerialHelper ComPort, String sOut) {
+        if (ComPort != null && ComPort.isOpen()) {
+            ComPort.sendTxt(sOut);
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CloseComPort(ComA);
+    }
+
 }
